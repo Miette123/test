6b6e869e0fa9581fdbe8858edf18915c9a491e0e
Myer <Myer@Lenovo-PC>
2018-09-20 21:01:47 +0800

    Initialize demo project

---
+++ src/main/java/com/example/demo/order/Order.java
@@ -0,0 +1,27 @@
+package com.example.demo.order;
+
+public class Order {
+	private Long id;
+	private String orderName;
+	private String orderTime;
+	public Long getId() {
+		return id;
+	}
+	public String getOrderName() {
+		return orderName;
+	}
+	public String getOrderTime() {
+		return orderTime;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setOrderName(String orderName) {
+		this.orderName = orderName;
+	}
+	public void setOrderTime(String orderTime) {
+		this.orderTime = orderTime;
+	}
+	
+	
+}
